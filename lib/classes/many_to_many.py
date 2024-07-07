41a8c2f818f9540fe9fea2d30b5855f83c0260fd
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-07 15:40:48 +0300

    Author class code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -6,19 +6,32 @@ class Article:
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Author's name must be a non-empty string.")
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        magazines = self.magazines()
+        if not magazines:
+            return None
+        return list({magazine.category for magazine in magazines})
+
 
 class Magazine:
     def __init__(self, name, category):
